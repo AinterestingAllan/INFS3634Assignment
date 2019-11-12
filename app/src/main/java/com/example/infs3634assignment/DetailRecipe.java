ea06d3609c364f8599d24616e224d15e44dfd1ae
Yuning CHU <z5185094@student.unsw.edu.au>
2019-11-12 19:34:48 +1100

    stopwatch

--- app/src/main/java/com/example/infs3634assignment/DetailRecipe.java
+++ app/src/main/java/com/example/infs3634assignment/DetailRecipe.java
@@ -8,10 +8,13 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.example.infs3634assignment.model.stopwatch;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
@@ -19,6 +22,7 @@ import java.util.ArrayList;
 public class DetailRecipe extends AppCompatActivity implements QuizMenuFragment.OnFragmentInteractionListener,
         ProfileFragment.OnFragmentInteractionListener, RecipeMenuFragment.OnFragmentInteractionListener
  {
+     public Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,6 +83,15 @@ public class DetailRecipe extends AppCompatActivity implements QuizMenuFragment.
 
         TextView DetailNutrition = findViewById(R.id.DetailNutrition);
         DetailNutrition.setText(builder.toString());
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), stopwatch.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
