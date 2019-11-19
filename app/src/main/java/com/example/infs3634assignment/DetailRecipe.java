00089b76ee7cdbdef2e6d3f1f0c4ae00a691fdbc
jenniferphung <42831614+jenniferphung@users.noreply.github.com>
2019-11-19 18:36:18 +1100

    Revert "remove logo from detail"
    This reverts commit 066db2913a2ae213ff8e18e3d639b8c5c16adca0.

--- app/src/main/java/com/example/infs3634assignment/DetailRecipe.java
+++ app/src/main/java/com/example/infs3634assignment/DetailRecipe.java
@@ -44,6 +44,26 @@ public class DetailRecipe extends AppCompatActivity implements QuizMenuFragment.
         String recipeServings = receivingServings;
         String recipeSource = receivingSource;
 
+        StringBuilder builder = new StringBuilder();
+        String calories = recipeNutrition.get(0).toString();
+        String amount1 = calories.substring(18, 23);
+        String protein = recipeNutrition.get(1).toString();
+        String amount2 = protein.substring(16, 21);
+        String fat = recipeNutrition.get(2).toString();
+        String amount3 = fat.substring(16, 21);
+        String carbs = recipeNutrition.get(3).toString();
+        String amount4 = carbs.substring(16, 21);
+
+        builder.append(
+                "Calories: " + amount1 + " cal" + "\n" +
+                        "Protein: " + amount2 + " g" + "\n" +
+                        "Fat: " + amount3 + " g" + "\n" +
+                        "Carbohydrates: " + amount4 + " g");
+
+        ConstraintLayout detailname = findViewById(R.id.Detail);
+        TextView titlename = detailname.findViewById(R.id.textView8);
+        titlename.setText(recipeTitle);
+
 
         ImageView DetailImage = findViewById(R.id.DetailImage);
         Glide.with(DetailImage.getContext()).load(recipeImage).into(DetailImage);
@@ -61,7 +81,7 @@ public class DetailRecipe extends AppCompatActivity implements QuizMenuFragment.
         DetailURL.setText("View Recipe: \n" + recipeURL + "\n");
 
         TextView DetailNutrition = findViewById(R.id.DetailNutrition);
-        DetailNutrition.setText("n/a");
+        DetailNutrition.setText(builder.toString());
 
         button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
