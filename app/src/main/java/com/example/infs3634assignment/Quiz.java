8f3ed948c2ad774f7c195d0899769daf5c522cc4
Yuning CHU <z5185094@student.unsw.edu.au>
2019-11-20 18:23:40 +1100

    quizcheck

--- app/src/main/java/com/example/infs3634assignment/Quiz.java
+++ app/src/main/java/com/example/infs3634assignment/Quiz.java
@@ -39,12 +39,23 @@ public class Quiz extends AppCompatActivity {
         final RadioButton Q1B2 = Q1Radio.findViewById(R.id.radioButton2);
         Q1B1.setText(quizCarbs.substring(quizCarbs.length() - 9)); //correct
         Q1B2.setText(quizProtein.substring(quizProtein.length() - 9));
-        final TextView checkAnswer = Q1.findViewById(R.id.checkAnswar);
-        Q1.setOnClickListener(new View.OnClickListener() {
+        final TextView checkAnswer = Q1Radio.findViewById(R.id.checkAnswar);
+        Q1B1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(Q1B1.isChecked()){
                     checkAnswer.setText("Correct!");
+                }else{
+                    checkAnswer.setText("Wrong!");
+                }
+            }
+        });
+
+        Q1B2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Q1B2.isChecked()){
+                    checkAnswer.setText("Wrong!");
                 }else{
                     checkAnswer.setText("Wrong!");
                 }
@@ -57,18 +68,29 @@ public class Quiz extends AppCompatActivity {
         Q2Question.setText("How many proteins are in " + quizTitle);
 
         RadioGroup Q2Radio = Q2.findViewById(R.id.Q1Radio);
-        RadioButton Q2B1 = Q2Radio.findViewById(R.id.radioButton1);
+        final RadioButton Q2B1 = Q2Radio.findViewById(R.id.radioButton1);
         final RadioButton Q2B2 = Q2Radio.findViewById(R.id.radioButton2);
         Q2B1.setText(quizFat.substring(quizFat.length() - 9));
         Q2B2.setText(quizProtein.substring(quizProtein.length() - 9)); //correct
-        final TextView checkAnswer2 = Q2.findViewById(R.id.checkAnswar);
-        Q2.setOnClickListener(new View.OnClickListener() {
+        final TextView checkAnswer2 = Q2Radio.findViewById(R.id.checkAnswar);
+        Q2B2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(Q2B2.isChecked()){
-                    checkAnswer.setText("Correct!");
+                    checkAnswer2.setText("Correct!");
                 }else{
-                    checkAnswer.setText("Wrong!");
+                    checkAnswer2.setText("Wrong!");
+                }
+            }
+        });
+
+        Q2B1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Q2B1.isChecked()){
+                    checkAnswer2.setText("Wrong!");
+                }else{
+                    checkAnswer2.setText("Wrong!");
                 }
             }
         });
@@ -80,20 +102,33 @@ public class Quiz extends AppCompatActivity {
 
         RadioGroup Q3Radio = Q3.findViewById(R.id.Q1Radio);
         final RadioButton Q3B1 = Q3Radio.findViewById(R.id.radioButton1);
-        RadioButton Q3B2 = Q3Radio.findViewById(R.id.radioButton2);
+        final RadioButton Q3B2 = Q3Radio.findViewById(R.id.radioButton2);
         Q3B1.setText(quizFat.substring(quizFat.length() - 9)); //correct
         Q3B2.setText(quizCarbs.substring(quizCarbs.length() - 9));
-        final TextView checkAnswer3 = Q3.findViewById(R.id.checkAnswar);
-        Q3.setOnClickListener(new View.OnClickListener() {
+        final TextView checkAnswer3 = Q3Radio.findViewById(R.id.checkAnswar);
+        Q3B1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(Q3B1.isChecked()){
-                    checkAnswer.setText("Correct!");
+                    checkAnswer3.setText("Correct!");
                 }else{
-                    checkAnswer.setText("Wrong!");
+                    checkAnswer3.setText("Wrong!");
                 }
             }
         });
+
+        Q3B2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Q3B2.isChecked()){
+                    checkAnswer3.setText("Wrong!");
+                }else{
+                    checkAnswer3.setText("Wrong!");
+                }
+            }
+        });
+
+
 
 
 
@@ -104,19 +139,30 @@ public class Quiz extends AppCompatActivity {
 
         RadioGroup Q4Radio = Q4.findViewById(R.id.Q1Radio);
         final RadioButton Q4B1 = Q4Radio.findViewById(R.id.radioButton1);
-        RadioButton Q4B2 = Q4Radio.findViewById(R.id.radioButton2);
+        final RadioButton Q4B2 = Q4Radio.findViewById(R.id.radioButton2);
         Q4B1.setText(quizGluten); //correct
         Boolean correctGluten = Boolean.parseBoolean(quizGluten);
         Boolean wrongGluten = !correctGluten;
         Q4B2.setText(wrongGluten.toString());
-        final TextView checkAnswer4 = Q4.findViewById(R.id.checkAnswar);
-        Q4.setOnClickListener(new View.OnClickListener() {
+        final TextView checkAnswer4 = Q4Radio.findViewById(R.id.checkAnswar);
+        Q4B1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(Q4B1.isChecked()){
-                    checkAnswer.setText("Correct!");
+                    checkAnswer4.setText("Correct!");
                 }else{
-                    checkAnswer.setText("Wrong!");
+                    checkAnswer4.setText("Wrong!");
+                }
+            }
+        });
+
+        Q4B2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Q4B2.isChecked()){
+                    checkAnswer4.setText("Wrong!");
+                }else{
+                    checkAnswer4.setText("Wrong!");
                 }
             }
         });
@@ -129,20 +175,31 @@ public class Quiz extends AppCompatActivity {
 
         RadioGroup Q5Radio = Q5.findViewById(R.id.Q1Radio);
         final RadioButton Q5B1 = Q5Radio.findViewById(R.id.radioButton1);
-        RadioButton Q5B2 = Q5Radio.findViewById(R.id.radioButton2);
+        final RadioButton Q5B2 = Q5Radio.findViewById(R.id.radioButton2);
 
         Q5B1.setText(quizDairy); //correct
         Boolean correctDairy = Boolean.parseBoolean(quizDairy);
         Boolean wrongDairy = !correctDairy;
         Q5B2.setText(wrongDairy.toString());
-        final TextView checkAnswer5 = Q5.findViewById(R.id.checkAnswar);
-        Q5.setOnClickListener(new View.OnClickListener() {
+        final TextView checkAnswer5 = Q5Radio.findViewById(R.id.checkAnswar);
+        Q5B1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(Q5B1.isChecked()){
-                    checkAnswer.setText("Correct!");
+                    checkAnswer5.setText("Correct!");
                 }else{
-                    checkAnswer.setText("Wrong!");
+                    checkAnswer5.setText("Wrong!");
+                }
+            }
+        });
+
+        Q5B2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Q5B2.isChecked()){
+                    checkAnswer5.setText("Wrong!");
+                }else{
+                    checkAnswer5.setText("Wrong!");
                 }
             }
         });
